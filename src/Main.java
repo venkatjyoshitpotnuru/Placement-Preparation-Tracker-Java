f167eb8d1a15146a525bb758a4d3f474cf51b437
jyoshit.ai_enthusiast <venkatjyoshitpotnuru7@gmail.com>
2026-06-22 11:31:06 +0530

    added weak area detection feature

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import model.StudentProgress;
+import service.ProgressService;
 
 public class Main {
-
     public static void main(String[] args) {
-
         StudentProgress progress = new StudentProgress(80, 75, 60, 90);
-
+        ProgressService service = new ProgressService();
+        double readinessScore = service.calculateReadinessScore(progress);
+        String weakArea = service.findWeakArea(progress);
         System.out.println(progress);
+        System.out.println("\nPlacement Readiness Score: "
+                + readinessScore + "%");
+        System.out.println("Weak Area: " + weakArea);
     }
 }
--- src/service/ProgressService.java
+++ src/service/ProgressService.java
@@ -1,5 +1,29 @@
 package service;
 
+import model.StudentProgress;
+
 public class ProgressService {
+    public double calculateReadinessScore(StudentProgress progress) {
+        double total = progress.getDsaScore() + progress.getJavaScore() +
+                progress.getAptitudeScore() + progress.getProjectScore();
+        return total / 4; // Assuming equal weightage for all subjects
+    }
+
+    public String findWeakArea(StudentProgress progress) {
+        int minScore = progress.getDsaScore();
+        String weakArea = "DSA";
+        if (progress.getJavaScore() < minScore) {
+            minScore = progress.getJavaScore();
+            weakArea = "Java";
+        }
+        if (progress.getAptitudeScore() < minScore) {
+            minScore = progress.getAptitudeScore();
+            weakArea = "Aptitude";
+        }
+        if (progress.getProjectScore() < minScore) {
+            weakArea = "Projects";
+        }
+        return weakArea;
+    }
 
 }
